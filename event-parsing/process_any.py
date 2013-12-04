e86bad07bfac6a73d21f2ba81a1d8885a595de6b
Sanae Rosen <sanae@umich.edu>
2013-12-03 21:33:19 -0500

    Updated to include preliminary analysis of power tracking.

--- event-parsing/process_any.py
+++ event-parsing/process_any.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, re, operator
+import sys, re, operator, os
 import robustnetLib, packet_analyzer
 from collections import Counter
 
@@ -8,6 +8,18 @@ from collections import Counter
 #	Total repeats of all
 # 	Interferers between pairs
 #	Port to 4G
+
+
+def reverseTime(t):
+	ms = t % 1000
+	t = t / 1000
+	s = t % 60
+	t = t / 60
+	m = t  %60
+	t = t /60
+	h = t
+	return str(h) + ":" +str(m) +":" + str(s) + "." + str(ms)
+	
 
 event_list = ["EVENT_LTE_BSR_SR_REQUEST", \
 	"EVENT_LTE_CM_OUTGOING_MSG", \
@@ -43,14 +55,22 @@ class Event:
 	current_event = None
 	last_state = None
 	distinct_events = set(["PACKET_SENT", "PACKET_RCV"])
+	power_ratio = None
+	RSSI = None
 
 	def __init__(self):
 		self.time = 0
 		self.event = None
+		self.event_line = None # for debugging
 		self.before_state = None
 		self.after_state = None
 		self.subtype = ""
 		self.secondary_attributes = {}
+		self.RSSI = Event.RSSI 
+		self.RSRP = None 
+		self.RSRQ = None 
+		self.power_ratio = Event.power_ratio
+		 
 
 	def addNewLine(self, line):
 		if line.startswith("2013"):
@@ -59,9 +79,11 @@ class Event:
 			self.__saveEvent()	
 			Event.current_event = Event()
 			Event.current_event.__getEvent(line)
-		Event.current_event.__getTime(line)
-		Event.current_event.__getStateChange(line)
-		Event.current_event.__getSecondary(line)
+		if Event.current_event:
+			Event.current_event.__getTime(line)
+			Event.current_event.__getStateChange(line)
+			Event.current_event.__getSignalStrengths(line)
+			Event.current_event.__getSecondary(line)
 
 	def addUpperLayerPackets(self, filename):
 		f = open(filename)
@@ -94,14 +116,20 @@ class Event:
 			self.time += int(match.group(2)) * 60 * 1000
 			self.time += int(match.group(1)) * 3600 * 1000
 
+
 	def __getEvent(self, line):
-		match = re.findall('[A-Z]+(?:_+[A-Z0-9]+)+', line)	
+		match = re.findall('[A-Z]+(?:_+[A-Z0-9]+)+', line)
+		self.event_line = line	
 		if len(match) > 0:
 			self.event = match[0]
 			if self.event == "EVENT_RRC_MESSAGE_RECEIVED":
 				self.subtype = " <---- "
 			elif self.event == "EVENT_RRC_MESSAGE_SENT":
-				self.subtype = " ----> " 
+				self.subtype = " ----> "
+		else:
+			line = line.split()
+			if len(line) > 6:
+				self.event = "_".join(line[6:])
 
 
 
@@ -189,18 +217,46 @@ class Event:
 					break
 				if match == None:
 					print "ERROR on ", line, self.event
-
-		
 				
 
-			
+	def __getSignalStrengths(self, line):
+#		if not self.event == "LTE ML1 Neighbor Measurements":
+#			return
+		line = line.split("|")
+		if len(line) < 10:
+			return
+		try:
+			# from LoadSense
+			if float(line[2]) >= 0 :
+				self.RSSI = float(line[3])
+				self.RSRP = float(line[4])
+				self.RSRQ = float(line[7])			
+			else:
+				self.RSSI = float(line[2])
+				self.RSRP = float(line[3])
+				self.RSRQ = float(line[6])			
+			self.power_ratio = self.RSRP/self.RSRQ
+			Event.RSSI = self.RSSI
+			Event.power_ratio = self.power_ratio
+			print line[2], line
+		except:
+			return
+
 
 	def __getStateChange(self, line):
 		if self.event== "EVENT_LTE_RRC_STATE_CHANGE" and line.startswith("Payload String"):
-			for state in lte_states:
-				if state in line:
-					self.after_state = state
-					break
+			match = re.search("RRC State = ([A-Za-z_ ]+)", line)
+			if match:
+				self.after_state = match.group(1)
+			else:
+				return
+			
+#			for state in lte_states:
+#				if state in line:
+#					self.after_state = state
+#					break
+			if Event.last_state == "Connecting" and self.after_state == "Closing":
+				print self.event_line
 			if Event.last_state == self.after_state:
 				return
 			self.before_state = Event.last_state
@@ -210,10 +266,11 @@ class Event:
 			if match:
 				self.before_state = match.group(1)
 				self.after_state = "CEL" + match.group(2)
-			match = re.search('Previous state: ([A-Za-z_]+), New state: ([A-Za-z_]+)', line)
+			match = re.search('Previous state: ([A-Za-z_ ]+), New state: ([A-Za-z_ ]+)', line)
 			if match:
 				self.before_state = match.group(1)
 				self.after_state = match.group(2)
+			Event.last_state = self.after_state
 				
 	def __saveEvent(self):
 		if Event.current_event == None or Event.current_event.event == None:
@@ -242,6 +299,7 @@ class Transition():
 
 #["radio_reconfig", "radio_reconfig_complete", "rlc_config", "cell_update", "update_confirm"]
 
+
 	def __create_dict(self, lists, d = False):
 		retval = {}
 		for v in Event.distinct_events:
@@ -269,10 +327,19 @@ class Transition():
 		self.duplicates_first = self.__create_dict(False)
 		self.duplicates_last = self.__create_dict(False)
 		self.duplicates_all = self.__create_dict(False)
+		self.RSSI = None
+		self.power_ratio = None 
 		interferers = {}	
 
 	def update(self, event):
-		#print event.before_state, event.after_state, event.event
+		#print event.before_state, event.after_state, event.event, reverseTime(event.time)
+		if event.RSSI != None:
+			self.RSSI = event.RSSI
+		print "RSSI", event.RSSI
+		if event.power_ratio != None:
+			self.power_ratio = event.power_ratio
+		#	print "power ratio", event.power_ratio
+			
 		if event.after_state != self.state and not event.event.startswith("PACKET"):
 			self.after_transition=  str(event.before_state) + " -> " + str(event.after_state)
 			self.end_time = event.time
@@ -287,7 +354,6 @@ class Transition():
 			self.state = event.after_state
 			if self.transition == None:
 				self.transition = str(event.before_state) + " -> " + str(event.after_state)
-			
 		return True
 
 	def find_stats_and_finalize(self, event):
@@ -300,10 +366,7 @@ class Transition():
 				# TODO update
 				self.duplicates_first[subtype] = count
 				self.attributes_first[subtype] = event.secondary_attributes
-			else:
-				self.duplicates_all[subtype] = 0
-				self.attributes_all[subtype] = {}
-					
+				
 #			event.printme()
 #			print "Finalizing!", self.end_time, event.time
 			self.time_to_reach_last[subtype] = self.end_time - event.time
@@ -311,6 +374,7 @@ class Transition():
 			self.attributes_last[subtype] = event.secondary_attributes
 			self.duplicates_all[subtype] += count
 			robustnetLib.mergeDict(event.secondary_attributes, self.attributes_all, subtype)
+		#print "Final RSSI", self.RSSI
 
 	def __print_attributes(self, attribute_dict, event):
 		if len(attribute_dict[event]) > 0:
@@ -336,6 +400,77 @@ class Transition():
 		new_l = [1 if x >= 1 else 0 for x in l]
 		return robustnetLib.meanValue(new_l)	
 			
+	def find_correlation(self, name, f_root):
+
+		f = None
+		if "camped -> connecting connecting -> connected" in name.lower():
+			occasionals = ["EVENT_LTE_EMM_INCOMING_MSG", "EVENT_LTE_EMM_TIMER_EXPIRY", "EVENT_LTE_RACH_ACCESS_START", "EVENT_LTE_EMM_TIMER_START"]
+			time_matters = [("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_RACH_RAID_MATCH"), ("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_MAC_TIMER"), ("EVENT_LTE_RACH_RAID_MATCH", "EVENT_LTE_RACH_ACCESS_RESULT"), ("EVENT_LTE_RRC_UL_MSG", "EVENT_LTE_RRC_DL_MSG"), ("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_RRC_UL_MSG"), ("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_RRC_PAGING_DRX_CYCLE")]
+			transition_type = "connecting"			
+		elif "connected -> closing closing ->" in name.lower():
+			occasionals = ["EVENT_LTE_ESM_OUTGOING_MSG", "EVENT_LTE_RACH_ACCESS_RESULT", "EVENT_LTE_UL_OUT_OF_SYNC", "EVENT_LTE_RACH_ACCESS_START", "EVENT_LTE_EMM_TIMER_START", "EVENT_LTE_EMM_INCOMING_MSG", "EVENT_LTE_CM_OUTGOING_MSG", "EVENT_LTE_RACH_RAID_MATCH", "EVENT_LTE_TIMING_ADVANCE", "EVENT_LTE_ML1_PHR_REPORT", "EVENT_LTE_BSR_SR_REQUEST", "EVENT_SLOTTED_MODE_OPERATION", "EVENT_LTE_ESM_OUTGOING_MSG", "EVENT_SD_EVENT_ACTION", "EVENT_IDLE_HANDOFF"]
+			time_matters = [("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_MAC_TIMER"), ("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_RRC_TIMER_STATUS"), ("EVENT_LTE_MAC_TIMER", "EVENT_LTE_RRC_TIMER_STATUS")]
+			transition_type = "closing"
+		elif "closing -> idle not camped idle not camped -> idle camped" in name.lower():
+
+			transition_type = "idle_nc"
+			occasionals = ["EVENT_LTE_EMM_TIMER_START", "EVENT_LTE_EMM_INCOMING_MSG", "EVENT_IPV6_SM_EVENT", "EVENT_LTE_RRC_DL_MSG", "EVENT_LTE_ESM_OUTGOING_MSG"]
+			time_matters = [("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_EMM_TIMER_START"), ("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_EMM_INCOMING_MSG"), ("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_RRC_TIMER_STATUS"), ("EVENT_LTE_RRC_STATE_CHANGE_TRIGGER", "EVENT_LTE_RRC_NEW_CELL_IND")]
+		elif "CELL_PCH -> CELL_FACH CELL_FACH -> CELL_DCH" in name:
+			transition_type = "fach_promote"
+			occasionals = ["CELL_UPDATE_MSG", "MEASUREMENT_REPORT_MSG"]
+			time_matters = [("SYSTEM_INFORMATION_MSG", "RADIO_BEARER_RECONFIGURATION_MSG"), ("RADIO_BEARER_RECONFIGURATION_MSG", "RADIO_BEARER_RECONFIGURATION_COMPLETE_MSG")]
+		elif "CELL_DCH -> CELL_FACH CELL_FACH -> CELL_DCH" in name:
+			transition_type = "fach_temp"
+			occasionals = []
+			time_matters = []
+		else:
+			return 
+
+		f = open(f_root + "_" + transition_type + ".txt", "a")
+		inter_time = self.end_time - self.begin_time
+		print >>f, inter_time,
+
+		occasionals_match = [False for i in occasionals]
+		for k, v in self.duplicates_all.iteritems():
+			for i in range(len(occasionals)):
+				if occasionals[i] in k and v > 0:
+					occasionals_match[i] = True
+
+		for item in occasionals_match:
+			if item:
+				print >>f, "1",
+			else:
+				print >>f, "0",
+			
+		print >>f, self.RSSI, self.power_ratio,
+
+		for t_pair in time_matters:
+			start = t_pair[0]
+			end = t_pair[1]
+					
+			if start in self.time_to_reach_first and end in self.time_to_reach_first:
+				print >>f, self.time_to_reach_first[end] - self.time_to_reach_first[start], 
+
+		if transition_type == "closing":
+			if "EVENT_LTE_MAC_TIMER" in self.time_to_reach_first:
+				print >>f, self.time_to_reach_first["EVENT_LTE_MAC_TIMER"] - self.time_to_reach_last["EVENT_LTE_MAC_TIMER"],
+			if "EVENT_LTE_RRC_TIMER_STATUS" in self.time_to_reach_first:
+				print >>f, self.time_to_reach_first["EVENT_LTE_RRC_TIMER_STATUS"] - self.time_to_reach_last["EVENT_LTE_RRC_TIMER_STATUS"],
+			if "EVENT_LTE_MAC_TIMER" in self.attributes_first:
+				if "Start" in self.attributes_first["EVENT_LTE_MAC_TIMER"]:
+					print >>f, "1",
+				else:
+					print >>f, "0",
+
+		if transition_type == "idle_nc":
+			if "EVENT_LTE_RRC_TIMER_STATUS" in self.attributes_first and "Timer Value" in self.attributes_first["EVENT_LTE_RRC_TIMER_STATUS"]:
+				print >>f, self.attributes_first["EVENT_LTE_RRC_TIMER_STATUS"]["Timer Value"],
+			else:
+				print >>f, -1,
+
+		print >>f
+		f.close()
 
 	def merge_dicts_and_print(self, l, name, transition_file):
 		DEVELOP = False
@@ -399,28 +534,30 @@ class Transition():
 			if max(duplicates_first[k]) == 0 and max(duplicates_last[k]) == 0:
 				continue
 			print "\t", k 
-			print "\t\tBEGIN: "
-                        print "\t\t   time from start:", robustnetLib.meanValue(time_to_reach_first[k])
-			print "\t\t   frequency appears: ", self.fraction_there(duplicates_first[k])
-			print "\t\t   duplicates:", robustnetLib.meanValue(duplicates_first[k])
-			print "\t\t   min appearances:", min(duplicates_first[k])
-			print "\t\t   number of tests:", len(duplicates_first[k])
+			print "\t\tBEGIN: ",
+                        print "time from start:", robustnetLib.meanValue(time_to_reach_first[k]),
+			print "frequency appears: ", self.fraction_there(duplicates_first[k]),
+			print "duplicates:", robustnetLib.meanValue(duplicates_first[k]),
+			print "min appearances:", min(duplicates_first[k]),
+			print "number of tests:", len(duplicates_first[k])
 			self.__print_attributes(attributes_first, k)
-			print "\t\tALL: "
-			print "\t\t    frequency appears:", self.fraction_there(duplicates_all[k])
-			print "\t\t    duplicates:", robustnetLib.meanValue(duplicates_all[k])
+			print "\t\tALL: ",
+			print "frequency appears:", self.fraction_there(duplicates_all[k]),
+			print "duplicates:", robustnetLib.meanValue(duplicates_all[k]),
+			print "number of tests:", len(duplicates_all[k])
 			self.__print_attributes(attributes_all, k)
-			print "\t\tEND: "
-			print "\t\t   time from end:", robustnetLib.meanValue(time_to_reach_last[k])
-			print "\t\t   frequency appears:", self.fraction_there(duplicates_last[k])
-			print "\t\t   duplicates:", robustnetLib.meanValue(duplicates_last[k]) 
-			print "\t\t   min appearances:", min(duplicates_last[k])
+			print "\t\tEND: ",
+			print "time from end:", robustnetLib.meanValue(time_to_reach_last[k]),
+			print "frequency appears:", self.fraction_there(duplicates_last[k]),
+			print "duplicates:", robustnetLib.meanValue(duplicates_last[k]),
+			print "min appearances:", min(duplicates_last[k])
+			print "RSSI and power ratio:", item.RSSI, item.power_ratio
 			
 			self.__print_attributes(attributes_last, k)
 
 f = open(sys.argv[1])
 
-in_relevant_section = False
+#in_relevant_section = False
 event_parser = Event()
 
 #########################################################################
@@ -434,11 +571,11 @@ for line in f:
 
 #	if line.startswith("2013"):
 #		in_relevant_section = True
-	if len(line) == 0:
-		in_relevant_section = not in_relevant_section
-		continue
-	if in_relevant_section:
-		event_parser.addNewLine(line)
+#	if len(line) == 0:
+#		in_relevant_section = not in_relevant_section
+#		continue
+#	if in_relevant_section:
+	event_parser.addNewLine(line)
 
 if len(sys.argv) > 2:
 	event_parser.addUpperLayerPackets(sys.argv[2])
@@ -503,8 +640,18 @@ for event in sorted_events:
 				transition_dict[name] = [transition]
 		transition = Transition(event.after_state, event.time)
 
+if transition_file:
+	for suffix in ["connecting", "closing", "idle_nc"]:
+		if os.path.isfile(sys.argv[3] + "_" + suffix + ".txt"):
+			os.remove(sys.argv[3] + "_" + suffix + ".txt")	
+
 for k, v in transition_dict.iteritems():
+
 	if "None" not in k:
 		v[0].merge_dicts_and_print(v, k, transition_file)
+
+	if transition_file:
+		for item in v:
+			item.find_correlation(k, sys.argv[3])
 
 
